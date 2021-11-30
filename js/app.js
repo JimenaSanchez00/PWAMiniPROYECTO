2253d934dc6d3a6fe7fb59c686758ead039c7fff
JimenaSanchez00 <53657409+JimenaSanchez00@users.noreply.github.com>
2021-11-30 10:06:09 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-if ('seviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('../sw.js').then(() =>{
             console.log('Soy el service worker registrado y funcionando.')
